2b48b05d0fe872fd03baa331ec547bb7824bc4af
aidmony <aidmony@gmail.com>
2014-12-29 12:23:06 -0600

    clean up

--- cmsc701final/bwt.py
+++
@@ -1,9 +0,0 @@
-def bwt(s):
-    """Apply Burrows-Wheeler transform to input string."""
-    assert "$" not in s, "Input string cannot contain null character ('\\0')"
-    s += "$"  # Add end of file marker
-    table = sorted(s[i:] + s[:i] for i in range(len(s)))  # Table of rotations of string
-    last_column = [row[-1:] for row in table]  # Last characters of each row
-    return "".join(last_column)  # Convert list of characters into string
-
-print bwt("banana")
